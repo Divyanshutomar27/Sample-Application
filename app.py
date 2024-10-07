b08e2f9dba839117c2d7232d79d536087b5214df
Divyanshu Tomar <divyanshutomar27@gmail.com>
2024-10-07 20:43:59 +0530

    correction done

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Divyanshu Tomar"
+    return "My first project is successful"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
